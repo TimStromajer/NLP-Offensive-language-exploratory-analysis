cc718a3901dc83e97ffd3d7f6bfd93a54342ccea
Rok <rp8257@student.uni-lj.si>
2021-04-22 16:58:48 +0200

    Updated TFIDF and added K-Means. Visualisation doesn't work yet

--- textProcessing.py
+++ textProcessing.py
@@ -1,10 +1,14 @@
-import nltk
-from nltk.stem import SnowballStemmer
+import re
 
+import nltk
+import pandas as pd
+from nltk.stem import SnowballStemmer
 import string
 
+stemmer = SnowballStemmer("english")
 
-def tokenization(raw):
+
+def tokenize(raw):
     text = raw.lower()
     # remove punctuation
     table = text.maketrans({key: None for key in string.punctuation})
@@ -12,10 +16,37 @@ def tokenization(raw):
     tokens = nltk.word_tokenize(text)
     return tokens
 
+
 def stemming(tokens):
-    stemmer = SnowballStemmer("english")
     stems = [(token, stemmer.stem(token)) for token in tokens]
     return stems
+
+
+def tokenize_and_stem(text):
+    # First tokenize by sentence, then by word to ensure that punctuation is caught as it's own token.
+    tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # Filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation).
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    stems = [stemmer.stem(t) for t in filtered_tokens]
+    return stems
+
+
+def vocabulary_frame(documents):
+    totalvocab_stemmed = []
+    totalvocab_tokenized = []
+    for i in documents:
+        allwords_stemmed = tokenize_and_stem(i)
+        totalvocab_stemmed.extend(allwords_stemmed)
+
+        allwords_tokenized = tokenize(i)
+        totalvocab_tokenized.extend(allwords_tokenized)
+
+    # Todo: fix length mismatch
+    vocab_frame = pd.DataFrame({'words': totalvocab_tokenized}, index=totalvocab_tokenized)
+    return vocab_frame
 
 ### use case
 # text = "John works at OBI."
