4a1228a4905bf3effd9dc085c96d022496eb3eef
marko <marko.p.krajinovic@gmail.com>
2021-05-15 19:05:08 +0200

    Implement intermediate saving

--- w2v_document_embeddings.py
+++ w2v_document_embeddings.py
@@ -5,6 +5,7 @@ import os
 import pickle
 import numpy as np
 from textProcessing import tokenize
+from lazy_load import lazy
 
 
 def read_document(file_path):
@@ -35,30 +36,51 @@ def embed_documents(model, posts):
     return embedded_posts
 
 
+def load_or_create(path, action):
+    if os.path.exists(path):
+        with open(path, "rb") as f:
+            print(f"Loaded from {path}")
+            output = pickle.load(f)
+    else:
+        output = action()
+        with open(path, "wb") as f:
+            pickle.dump(output, f)
+            print(f"Generated and saved to {path}")
+    return output
+
+
 intermediate_location = f"{os.path.basename(__file__)}-intermediate_data"
 if not os.path.exists(intermediate_location):
     os.mkdir(intermediate_location)
 
-print(intermediate_location)
 
 if __name__ == '__main__':
-    print("Loading word2vec...")
-    model_gn: gensim.models.Word2Vec = gensim.downloader.load('word2vec-google-news-300')
-    print("loaded")
+    def load_w2v():
+        print("Loading word2vec...")
+        w2v_model = gensim.downloader.load('word2vec-google-news-300')
+        print("Loaded.")
+        return w2v_model
+    model = lazy(load_w2v)
+    input_files = ['9.csv',
+                   '21.csv',
+                   '25.csv',
+                   '26.csv',
+                   '31.csv',
+                   '32.csv',
+                   'jigsaw-toxic.csv'
+                   ]
 
-    data_path = 'data'
-    file_name = 'jigsaw-toxic.csv'
-    file_path = os.path.join(data_path, file_name)
-    data = read_document(file_path)
-    posts = list(data['text'])
-    labels = list(data['class'])
+    for file_name in input_files:
+        data_path = 'data'
+        file_path = os.path.join(data_path, file_name)
+        data = read_document(file_path)
+        posts = list(data['text'])
+        labels = list(data['class'])
 
-    embedding_file_path = os.path.join(intermediate_location, f"{file_name}-embeddings.p")
+        embedding_file_path = os.path.join(intermediate_location, f"{file_name}-embeddings.p")
+        post_embeddings = load_or_create(embedding_file_path, lambda: embed_documents(model, posts))
 
-    post_embeddings = embed_documents(model_gn, posts)
-    with open(embedding_file_path, "wb") as f:
-        pickle.dump(post_embeddings, f)
-
+        print(len(post_embeddings))
     # tokens = tokenize(data)
     # embedding = model_gn[tokens[0]]
     #
