cf6151d6ecf16ebbd66594b925dad38f12b37507
marko <marko.p.krajinovic@gmail.com>
2021-05-19 10:07:14 +0200

    Create MDA visualization for word2vec total embeddings

--- w2v_document_embeddings.py
+++ w2v_document_embeddings.py
@@ -14,7 +14,33 @@ from torch import mean, argsort
 from sentence_transformers.util import pytorch_cos_sim
 from nltk.corpus import stopwords
 
+
+from sklearn.manifold import TSNE
+from sklearn.manifold import MDS
+from sklearn.decomposition import PCA
+from statistics import mean
+pd.options.display.max_columns = None
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from sentence_transformers.util import pytorch_cos_sim
+
+
+
+
 stopwords = stopwords.words('english')
+
+
+def cart2pol(x, y):
+    rho = np.sqrt(x**2 + y**2)
+    phi = np.arctan2(y, x)
+    return(rho, phi)
+
+
+def pol2cart(rho, phi):
+    x = rho * np.cos(phi)
+    y = rho * np.sin(phi)
+    return(x, y)
+
 
 def read_document(file_path):
     return pd.read_csv(file_path, dtype=str, usecols=[1, 2])
@@ -22,18 +48,18 @@ def read_document(file_path):
 
 def embed_document(model, tokens):
     total_vector = np.zeros([model.vector_size], dtype=np.float32)
-    # normalization = 0
+    normalization = 0
     for token in tokens:
         if token in stopwords:
-            print(f"skipped: {token}")
+            # print(f"skipped: {token}")
             continue
         if token in model:
             embedding = model[token]
             total_vector += embedding
-    #         normalization += 1
-    # if normalization == 0:
-    #     return total_vector
-    return total_vector# /normalization
+            normalization += 1
+    if normalization == 0:
+        return total_vector
+    return total_vector / normalization
 
 
 def embed_documents(model, posts):
@@ -75,6 +101,60 @@ def document_group_similarities(a_embeddings, b_embeddings):
     a_to_b = mean(a_to_b_averages)
     b_to_a = mean(b_to_a_averages)
     return a_to_b, b_to_a, top_a_to_b, top_b_to_a
+
+
+def plot_similar_words(title, labels, embedding_clusters, filename=None):
+    plt.figure(figsize=(16, 9))
+    colors = cm.rainbow(np.linspace(0, 1, len(labels)))
+    for label, embeddings, color in zip(labels, embedding_clusters, colors):
+        x = embeddings[:, 0]
+        y = embeddings[:, 1]
+        plt.scatter(x, y, c=color, alpha=0.7, label=label)
+        plt.annotate(label.upper(), alpha=1.0, xy=(mean(x), mean(y)), xytext=(0, 0),
+                     textcoords='offset points', ha='center', va='center', size=15)
+    plt.legend(loc=4)
+    plt.title(title)
+    plt.grid(False)
+    if filename:
+        plt.savefig(filename, format='png', dpi=150, bbox_inches='tight')
+    plt.show()
+
+
+def plotTSNE(title, embedding_clusters, perplexity=15, filename=None):
+    embedding_clusters = np.array(embedding_clusters)
+    n, m, k = embedding_clusters.shape
+    print("Performing TSNE")
+    model_en_2d = TSNE(perplexity=perplexity, n_components=2, init='pca', n_iter=3500, random_state=32)
+    model_en_2d = model_en_2d.fit_transform(embedding_clusters.reshape(n * m, k))
+    embeddings_en_2d = np.array(model_en_2d).reshape(n, m, 2)
+    plot_similar_words(title, [SPEECH_CLASSES[int(label)] for label in labels], embeddings_en_2d, filename)
+
+
+
+def plotMDS(title, embedding_clusters, filename = None):
+    embedding_clusters = np.array(embedding_clusters)
+    n, m, k = embedding_clusters.shape
+    print("Performing MDS")
+    model_en_2d = MDS(n_components=2, max_iter=3500, random_state=32)
+    model_en_2d = model_en_2d.fit_transform(embedding_clusters.reshape(n * m, k))
+    embeddings_en_2d = np.array(model_en_2d).reshape(n, m, 2)
+    plot_similar_words(title, [SPEECH_CLASSES[int(label)] for label in labels], embeddings_en_2d, filename)
+
+
+def plotPCA(title, embedding_clusters, filename = None):
+    embedding_clusters = np.array(embedding_clusters)
+    n, m, k = embedding_clusters.shape
+    model_en_2d = PCA(n_components=2, random_state = 32)
+    model_en_2d = model_en_2d.fit_transform(embedding_clusters.reshape(n * m, k))
+    embeddings_en_2d = np.array(model_en_2d).reshape(n, m, 2)
+    embeddings_en_2d = [cart2pol(v[0][0], v[0][1]) for v in embeddings_en_2d.tolist()]
+    max_phi = max(embeddings_en_2d, key=lambda x: x[1])[1]
+    min_phi = min(embeddings_en_2d, key=lambda x: x[1])[1]
+    print(max_phi)
+    print(min_phi)
+    embeddings_en_2d = [[pol2cart(r, p)] for r, p in embeddings_en_2d]
+    embeddings_en_2d = np.array(embeddings_en_2d)
+    plot_similar_words(title, [SPEECH_CLASSES[int(label)] for label in labels], embeddings_en_2d, filename)
 
 
 intermediate_location = f"{os.path.basename(__file__)}-intermediate_data"
@@ -133,7 +213,7 @@ if __name__ == '__main__':
     print([SPEECH_CLASSES[int(label)] for label in labels])
 
     top_count_print = 5
-    top_count_visualize = 100
+    top_count_visualize = 30
 
     # Centroid based representative
     embedding_totals = list()
@@ -142,6 +222,7 @@ if __name__ == '__main__':
 
     for label in labels_embeddings:
         combined = sum(labels_embeddings[label])
+        combined = combined/len(labels_embeddings[label])
         embedding_totals.append(combined)
         count = len(labels_embeddings[label])
         centroid = combined/count
@@ -162,13 +243,20 @@ if __name__ == '__main__':
         for i in range(top_count_visualize):
             label_embedding_top.append(labels_embeddings[label][sort[i]])
             label_post_top.append(label_posts[label][sort[i]])
+        embedding_top_similar.append(label_embedding_top)
+        posts_top_similar.append(label_post_top)
 
     #Centroid based similarity
     similarity = pytorch_cos_sim(embedding_totals, embedding_totals)
     print([SPEECH_CLASSES[int(label)] for label in labels])
     print(similarity)
 
-    
+
+    plotMDS("Totals", [[tot] for tot in embedding_totals])
+    # plotMDS("Title", embedding_top_similar)
+    # plotTSNE("Title", embedding_top_similar, perplexity=10)
+    # plotPCA("Title", embedding_top_similar)
+
 
     # Save similarities to file
     # with open("distances.csv", 'w', newline='') as f:
